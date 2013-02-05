e385a28376e31a3f08a2ce23c22682d59d7deb12
Andrew Braunstein <awbraunstein@gmail.com>
2013-02-05 15:22:37 -0500

    connected all buttons and now they are updating the label

--- Calculator/AWBViewController.h
+++ Calculator/AWBViewController.h
@@ -9,5 +9,29 @@
 #import <UIKit/UIKit.h>
 
 @interface AWBViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UILabel *echoArea;
+- (IBAction)openParen:(id)sender;
+- (IBAction)closeParen:(id)sender;
+- (IBAction)pi:(id)sender;
+- (IBAction)euler:(id)sender;
+- (IBAction)clear:(id)sender;
+- (IBAction)sqrt:(id)sender;
+- (IBAction)divide:(id)sender;
+- (IBAction)times:(id)sender;
+- (IBAction)minus:(id)sender;
+- (IBAction)plus:(id)sender;
+- (IBAction)equals:(id)sender;
+- (IBAction)dot:(id)sender;
+- (IBAction)zero:(id)sender;
+- (IBAction)one:(id)sender;
+- (IBAction)two:(id)sender;
+- (IBAction)three:(id)sender;
+- (IBAction)four:(id)sender;
+- (IBAction)five:(id)sender;
+- (IBAction)six:(id)sender;
+- (IBAction)seven:(id)sender;
+- (IBAction)eight:(id)sender;
+- (IBAction)nine:(id)sender;
+
 
 @end
--- Calculator/AWBViewController.m
+++ Calculator/AWBViewController.m
@@ -23,6 +23,8 @@
   
   UIImage * patternImage = [UIImage imageNamed:@"use_your_illusion.png"];
   self.view.backgroundColor = [UIColor colorWithPatternImage:patternImage];
+  
+  
 }
 
 - (void)didReceiveMemoryWarning
@@ -31,4 +33,121 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (void)appendToEchoArea:(NSString*)string {
+  [self.echoArea setText:[self.echoArea.text stringByAppendingString:string]];
+}
+
+- (void)clearEchoArea {
+  [self.echoArea setText:@""];
+}
+
+- (IBAction)openParen:(id)sender {
+  NSLog(@"(");
+  [self appendToEchoArea:@"("];
+}
+
+- (IBAction)closeParen:(id)sender {
+  NSLog(@")");
+  [self appendToEchoArea:@")"];
+}
+
+- (IBAction)pi:(id)sender {
+  NSLog(@"π");
+  [self appendToEchoArea:@"π"];
+}
+
+- (IBAction)euler:(id)sender {
+  NSLog(@"e");
+  [self appendToEchoArea:@"e"];
+}
+
+- (IBAction)clear:(id)sender {
+  NSLog(@"CLEAR");
+  [self clearEchoArea];
+}
+
+- (IBAction)sqrt:(id)sender {
+  NSLog(@"√");
+  [self appendToEchoArea:@"√"];
+}
+
+- (IBAction)divide:(id)sender {
+  NSLog(@"÷");
+  [self appendToEchoArea:@"÷"];
+  
+}
+
+- (IBAction)times:(id)sender {
+  NSLog(@"×");
+  [self appendToEchoArea:@"×"];
+}
+
+- (IBAction)minus:(id)sender {
+  NSLog(@"-");
+  [self appendToEchoArea:@"-"];
+}
+
+- (IBAction)plus:(id)sender {
+  NSLog(@"+");
+  [self appendToEchoArea:@"+"];
+}
+
+- (IBAction)equals:(id)sender {
+  NSLog(@"EVALUATE");
+}
+
+- (IBAction)dot:(id)sender {
+  NSLog(@".");
+  [self appendToEchoArea:@"."];
+}
+
+- (IBAction)zero:(id)sender {
+  NSLog(@"0");
+  [self appendToEchoArea:@"0"];
+}
+
+- (IBAction)one:(id)sender {
+  NSLog(@"1");
+  [self appendToEchoArea:@"1"];
+}
+
+- (IBAction)two:(id)sender {
+  NSLog(@"2");
+  [self appendToEchoArea:@"2"];
+}
+
+- (IBAction)three:(id)sender {
+  NSLog(@"3");
+  [self appendToEchoArea:@"3"];
+}
+
+- (IBAction)four:(id)sender {
+  NSLog(@"4");
+  [self appendToEchoArea:@"4"];
+}
+
+- (IBAction)five:(id)sender {
+  NSLog(@"5");
+  [self appendToEchoArea:@"5"];
+}
+
+- (IBAction)six:(id)sender {
+  NSLog(@"6");
+  [self appendToEchoArea:@"6"];
+}
+
+- (IBAction)seven:(id)sender {
+  NSLog(@"7");
+  [self appendToEchoArea:@"7"];
+}
+
+- (IBAction)eight:(id)sender {
+  NSLog(@"8");
+  [self appendToEchoArea:@"8"];
+}
+
+- (IBAction)nine:(id)sender {
+  NSLog(@"9");
+  [self appendToEchoArea:@"9"];
+}
 @end
